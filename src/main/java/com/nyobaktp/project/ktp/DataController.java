4c55b1930064ae0c3499532ea666bddace0518af
Xcused <zulvafad@gmail.com>
2022-05-20 22:56:39 +0700

    first commit

---
+++ src/main/java/com/nyobaktp/project/ktp/DataController.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.nyobaktp.project.ktp;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+/**
+ *
+ * @author Lenovo
+ */
+@Controller
+public class DataController {
+    
+    DataJpaController datactrl = new DataJpaController();
+    List<Data> newdata = new ArrayList<>();
+    
+    @RequestMapping("/main")
+    public String getMain() {
+        return "menu";
+    }
+    
+   public String getDataKTP(Model model){
+        int record = datactrl.getDataCount();
+        String result="";
+        try{
+            newdata = datactrl.findDataEntities().subList(0, record);
+        }
+        catch(Exception e){result=e.getMessage();}
+        model.addAttribute("goData", newdata);
+        model.addAttribute("record", record);
+        return "database";
+        
+    }
+    
+    @RequestMapping("/edit")
+    public String doEdit(){
+        return "editktp";
+    }
+}
---
+++ src/main/java/com/nyobaktp/project/ktp/exceptions/PreexistingEntityException.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.nyobaktp.project.ktp.exceptions;
+
+public class PreexistingEntityException extends Exception {
+    public PreexistingEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public PreexistingEntityException(String message) {
+        super(message);
+    }
+}
